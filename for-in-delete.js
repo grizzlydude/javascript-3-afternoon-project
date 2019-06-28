738609d07024d1dafc56d6967c382de06644212a
Grant <gwensignak@gmail.com>
2019-06-28 10:22:11 -0600

    second part done

--- for-in-delete.js
+++ for-in-delete.js
@@ -40,7 +40,11 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  let nope = ''
+  for (let key in obj){
+    nope+= obj[key]
+  }
+  return nope
 }
 
 
@@ -53,7 +57,14 @@ function showValues( obj ) {
   Return the updated object.
 */
 
-//Code Here
+function greaterThan10(obj){
+  for (let key in obj){
+    if (obj[key] > 10){
+      obj[key] = 0
+    }
+  }
+  return obj
+}
 
 
 
@@ -65,7 +76,12 @@ function showValues( obj ) {
   Return the updated object.
 */
 
-//Code Here
+function double(obj){
+  for (let key in obj){
+    obj[key] = obj[key] * 2
+  }
+  return obj
+}
 
 
 
@@ -79,7 +95,15 @@ function showValues( obj ) {
   By the end of the for in loop, you should have a sentence, return that sentence.
 */
 
-//Code Here
+function secrets (obj){
+  let sent = ''
+  for (let key in obj){
+    if (key.startsWith('sh')){
+      sent += obj[key]
+    }
+  }
+  return sent
+}
 
 
 
@@ -110,7 +134,10 @@ function showValues( obj ) {
   Delete the property password and return the object.
 */
 
-//Code Here
+function removePassword(obj){
+  delete obj.password
+  return obj
+}
 
 
 
@@ -129,7 +156,11 @@ var deleteTheBigNumbers = {
   Write a for in loop that deletes every property from the object deleteTheBigNumbers whose value is greater than 100.
 */
 
-//Code Here
+for (let key in deleteTheBigNumbers){
+  if (deleteTheBigNumbers[key] > 100){
+    delete deleteTheBigNumbers[key]
+  }
+}
 
 
 
@@ -142,7 +173,14 @@ var deleteTheBigNumbers = {
   Return the updated object.
 */
 
-//Code Here
+function startsWithK(obj){
+  for (let key in obj){
+    if (key.startsWith('k')){
+      delete obj[key]
+    }
+  }
+  return obj
+}
 
 
 
@@ -157,6 +195,15 @@ var deleteTheBigNumbers = {
   (hint: the method includes() may be of use...)
 */
 
-//Code Here
+function hiddenTreasure(obj){
+  for (let key in obj){
+    if (obj[key].includes('treasure')){
+    }
+    else{
+      delete obj[key]
+    }
+  }
+  return obj
+}
 
 
