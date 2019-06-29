da971a1a9c4a9d1a9dc5f4afc3b7c8a683db88d6
Grant <gwensignak@gmail.com>
2019-06-29 15:56:54 -0600

    Third part done

--- higher-order.js
+++ higher-order.js
@@ -137,10 +137,10 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-// let bobsTotal = purches.reduce((acc, el)=> {
-//   return acc + el.price
-// }, 0)
-const bobsTotal= purchases.reduce(function(acc, el, i, arr){
-  return acc + el.price
-}, }
+const bobsTotal= purchases.reduce(function(acc, el, i){
+  if (purchases[i].owner == 'Bob'){
+    return acc + purchases[i].price
+  }
+  return acc
+}, 0)
 
